ffef5a3c232cb1336f67c4d8d6f803a83090d0d0
Isaac Jordan <isaaz.jordan02@gmail.com>
2025-05-28 22:17:58 -0300

    Refactor: Centralize Service and Middleware Configuration in Program.cs

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,18 +1,46 @@
+// Load environment variables in Run Time
+DotNetEnv.Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Configure services
+ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+// Configure middleware
+ConfigureMiddleware(app);
 
 app.Run();
+
+// Command-line reference:
+// dotnet ef migrations add InitialCreate -p Infrastructure -s Presentation
+// dotnet ef database update -p Infrastructure -s Presentation
+
+void ConfigureServices(IServiceCollection services)
+{
+    var connectionString = Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING");
+
+    if (string.IsNullOrWhiteSpace(connectionString))
+        throw new InvalidOperationException("MYSQL_CONNECTION_STRING is not set in the environment variables (In Run Time).");
+
+    services.AddDbContext<AppDbContext>(options =>
+        options.UseMySql(
+            connectionString,
+            new MySqlServerVersion(new Version(8, 0, 36))
+        ));
+
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+}
+
+void ConfigureMiddleware(WebApplication app)
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+}
