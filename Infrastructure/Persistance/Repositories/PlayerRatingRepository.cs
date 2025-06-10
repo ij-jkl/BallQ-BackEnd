742bc22ea80c0dee22fb03189e84f0b52ea45d68
Isaac Jordan <isaaz.jordan02@gmail.com>
2025-06-09 21:35:19 -0300

    Add : More methods to PlayerRatingRepository, such as get by id, get all, and filter by top or bottom players.

--- Infrastructure/Persistance/Repositories/PlayerRatingRepository.cs
+++ Infrastructure/Persistance/Repositories/PlayerRatingRepository.cs
@@ -36,4 +36,34 @@
 
         await _context.SaveChangesAsync();
     }
+    
+    public async Task<RatingEntity?> GetById(int id)
+    {
+        return await _context.Ratings
+            .Include(r => r.Player) 
+            .FirstOrDefaultAsync(r => r.Id == id);
+    }
+
+    
+    public IQueryable<RatingEntity> AsQueryable()
+    {
+        return _context.Ratings.AsQueryable()
+            .Include(r => r.Player);
+    }
+    
+    public async Task<List<RatingEntity>> GetTopRatingsAsync(int limit, bool ascending, CancellationToken cancellationToken)
+    {
+        var query = _context.Ratings
+            .Include(r => r.Player)
+            .AsQueryable();
+
+        query = ascending
+            ? query.OrderBy(r => r.FinalScore)
+            : query.OrderByDescending(r => r.FinalScore);
+
+        return await query
+            .Take(limit)
+            .ToListAsync(cancellationToken);
+    }
+
 }
