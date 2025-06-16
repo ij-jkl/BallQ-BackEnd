24dbd523db8541892800ac640bc5af1e3864025f
Isaac Jordan <isaaz.jordan02@gmail.com>
2025-06-16 16:30:21 -0300

    Add : Validators and Middleware for command validation.

---
+++ Application/Validation/RatePlayers/RateAllStrikersCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Validation.RatePlayers;
+
+public class RateAllStrikersCommandValidator : AbstractValidator<RateAllStrikersCommand>
+{
+    public RateAllStrikersCommandValidator()
+    {
+        RuleFor(_ => _)
+            .NotNull()
+            .WithMessage("RateAllStrikersCommand cannot be null.");
+    }
+}
---
+++ Presentation/Middleware/ValidationExceptionMiddleware.cs
@@ -0,0 +1,35 @@
+﻿namespace Presentation.Middleware;
+
+public class ValidationExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ValidationExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (ValidationException ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            context.Response.ContentType = "application/json";
+
+            var errorMessages = ex.Errors.Select(e => $"{e.PropertyName}: {e.ErrorMessage}").ToList();
+
+            var response = new ResponseObjectJsonDto
+            {
+                Code = 400,
+                Message = "Validation failed.",
+                Response = errorMessages
+            };
+
+            await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+    }
+}
